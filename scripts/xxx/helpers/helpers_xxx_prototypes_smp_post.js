cbfd2d003dd8f4f8bb62617dc53d337e4acd01be
a1waysbeta <2366650060@qq.com>
2025-12-18 16:50:04 +0800

    Add files via upload

---
+++ scripts/xxx/helpers/helpers_xxx_prototypes_smp_post.js
@@ -0,0 +1,45 @@
+﻿'use strict';
+//24/11/25
+
+if (typeof addEventListener !== 'undefined' && typeof removeEventListenerSelf !== 'undefined') {
+	const playbackHistory = [];
+	const addToPlaybackHistory = (handle) => {
+		const pl = plman.GetPlayingItemLocation();
+		const item = {
+			handle,
+			idx: pl.IsValid ? pl.PlaylistItemIndex : -1,
+			plsIdx: pl.IsValid ? pl.PlaylistIndex : -1,
+			plsName: pl.IsValid && pl.PlaylistIndex !== -1 ? plman.GetPlaylistName(pl.PlaylistIndex) : null,
+			plsGUID: pl.IsValid && pl.PlaylistIndex !== -1 ? plman.GetGUID(pl.PlaylistIndex) : null
+		};
+		if (playbackHistory.unshift(item) > 10) { playbackHistory.pop(); }
+	};
+
+	addEventListener('on_playback_new_track', addToPlaybackHistory);
+
+	fb.GetPrevPlaying = (idx = fb.IsPlaying ? 1 : 0) => {
+		if (typeof idx !== 'number' || idx < 0) { idx = 0; }
+		const prev = playbackHistory[idx];
+		if (prev) { // Refresh all data preferring old matches
+			if (prev.plsGUID) {
+				if (plman.GetGUID(prev.plsIdx) !== prev.plsGUID) { prev.plsIdx = plman.FindByGUID(prev.plsGUID); }
+				if (prev.plsIdx !== -1) { prev.plsName = plman.GetPlaylistName(prev.plsIdx); };
+			} else if (plman.GetPlaylistName(prev.plsIdx) !== prev.plsName) {
+				prev.plsIdx = plman.FindPlaylist(prev.plsName);
+			}
+			if (prev.plsIdx !== -1) {
+				const plsItems = plman.GetPlaylistItems(prev.plsIdx).Convert();
+				if (plsItems[idx] !== prev.handle) {
+					let i = 0;
+					for (let handle of plsItems) {
+						if (handle.Compare(prev.handle)) { prev.idx = i; break; }
+						i++;
+					}
+				}
+			}
+		}
+		return prev || null;
+	};
+
+	if (fb.IsPlaying) { addToPlaybackHistory(fb.GetNowPlaying()); }
+}
